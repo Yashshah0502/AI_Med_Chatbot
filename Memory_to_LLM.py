074a85b1f2efde8ae6841d1f43fa88842daa9bde
Yashshah0502 <113469955+Yashshah0502@users.noreply.github.com>
2025-07-12 18:39:05 -0400

    Momory_to_llm

---
+++ Memory_to_LLM.py
@@ -0,0 +1,12 @@
+import os
+
+from langchain_huggingface import HuggingFaceEndpoint
+from langchain_core.prompts import PromptTemplate
+from langchain.chains import RetrievalQA
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv())
+
+
